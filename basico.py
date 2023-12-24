8f9f53f30d0b2e06b7f678181bcb49984d6d6866
PatricioFernandezF <patricio.fernandez.florez@gmail.com>
2023-12-24 18:58:27 +0100

    Lo mas basico

---
+++ basico.py
@@ -0,0 +1,23 @@
+import json
+from urllib import request, parse
+import random
+
+# ======================================================================
+# This function sends a prompt workflow to the specified URL 
+# (http://127.0.0.1:8188/prompt) and queues it on the ComfyUI server
+# running at that address.
+def queue_prompt(prompt_workflow):
+    p = {"prompt": prompt_workflow}
+    data = json.dumps(p).encode('utf-8')
+    req =  request.Request("http://127.0.0.1:8188/prompt", data=data)
+    request.urlopen(req)    
+# ======================================================================
+
+# read workflow api data from file and convert it into dictionary 
+# assign to var prompt_workflow
+prompt_workflow = json.load(open('vino.json'))
+
+
+
+# everything set, add entire workflow to queue.
+queue_prompt(prompt_workflow)
